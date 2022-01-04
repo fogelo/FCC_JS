fc6ab2118fee0a99ebaa1bc26afafd93823cd71b
antonorlov <fogelo@yandex.rru>
2022-01-04 21:22:40 +0300

    task 1. Iterate Odd Numbers With a For Loop2

--- script.js
+++ script.js
@@ -100,10 +100,24 @@ function updateRecords(records, id, prop, value) {
 
 /*!3.Iterate Odd Numbers With a For Loop*/
 
-const myArray = [];
+/*const myArray = [];
 
 // Only change code below this line
 
 for(let i = 1; i<=5;i++) {
     myArray.push(i)
+}*/
+
+/*!4.Iterate Odd Numbers With a For Loop2*/
+
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 1;i<=9;i+=2) {
+    myArray.push(i)
 }
+
+console.log(myArray)
+
+

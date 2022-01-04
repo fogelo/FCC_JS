7deb9134eecde11688422eff3c19259070d25918
antonorlov <fogelo@yandex.rru>
2022-01-04 21:01:48 +0300

    task 1. Record Collection

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //§ FCC_JS
 
 /*!1. Counting Cards*/
+
+/*
 let count = 0;
 
 function cc(card) {
@@ -49,3 +51,49 @@ function cc(card) {
 cc(10); cc("J"); cc("K"); cc('Q'); cc('A');
 
 console.log(count)
+*/
+
+/*!2. Record Collection*/
+
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+function updateRecords(records, id, prop, value) {
+
+    if (prop === 'artist') {
+        records[id][prop] = value
+    }
+    if (prop === 'tracks' && value === "Take a Chance on Me") {
+        records[id][prop] = []
+        records[id].tracks.push(value)
+    }
+    if (value === '') {
+        delete records[id][prop]
+    }
+    if (prop === 'tracks' && value === 'Addicted to Love') {
+        records[id].tracks.push(value)
+    }
+    if (prop === 'albumTitle' && value === 'Riptide'){
+        records[id][prop] = value
+    }
+        return records;
+}
+
+
